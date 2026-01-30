9111cc17bd48bafc3433d9fe98abbd27a1ae8217
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-30 18:09:07 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ connectDB()
 
 // CORS configuration
 const corsOptions = {
-  origin: ["http://54.226.153.201:5173", "http://54.226.153.201:5174","http://54.226.153.201:5173"],// React dev server
+  origin: ["http://54.226.153.201:5173", "http://54.226.153.201:5174"],// React dev server
   //origin: process.env.FRONTEND_URLS.split(','),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
