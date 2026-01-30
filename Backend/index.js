fc987ce89c5dd6cf0c7de7813820b6c3616654a0
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-30 21:20:10 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -25,8 +25,8 @@ const corsOptions = {
   //origin: ["http://10.100.164.102:5173", "http://10.100.164.102:5174"],// React dev server
   //origin: process.env.FRONTEND_URLS.split(','),
   origin: [
-    "http://ad2da1cfbfd9d40438bc552b503cab79-1330930703.us-east-1.elb.amazonaws.com:5173",
-    "http://ad2da1cfbfd9d40438bc552b503cab79-1330930703.us-east-1.elb.amazonaws.com:5174"
+    "http://abab0119efd6047bda3c46cf49fadf3b-1770816035.us-east-1.elb.amazonaws.com:5173",
+    "http://abab0119efd6047bda3c46cf49fadf3b-1770816035.us-east-1.elb.amazonaws.com:5174"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
