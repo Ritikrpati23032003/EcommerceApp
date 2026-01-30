9ed7330e436bf0dbc85ed353dc8bc37c9468204e
Ritik Ranjan Pati <ritikranjanpati@gmail.com>
2026-01-30 22:05:13 +0530

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       process.env.MONGODB_URI ||
-        "mongodb://admin:7325APM@abab0119efd6047bda3c46cf49fadf3b-1770816035.us-east-1.elb.amazonaws.com:27017/ecommerce?authSource=admin",
+        "mongodb://admin:7325APM@mongo:27017/ecommerce?authSource=admin",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
